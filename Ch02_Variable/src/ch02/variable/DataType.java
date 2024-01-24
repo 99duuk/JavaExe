cda3d5edbc72cf99c46f6e6ee38f8b7f6c70b40a
99duuk <99duuk@gmail.com>
2024-01-24 15:44:30 +0900

    name1 추가 출력

--- Ch02_Variable/src/ch02/variable/DataType.java
+++ Ch02_Variable/src/ch02/variable/DataType.java
@@ -10,6 +10,7 @@ public class DataType {
 		boolean isRun = true; 			// true/false only 논리표현하기위한~
 		String str = "a";				// 문자열 (여러 글자중 하나 들어 갔을 뿐이야~
 		//다른 건 한 번 찾아보고 넘어가세여~ 
+		String name1 = "Albert"; 		//문자열
 		
 		System.out.println(num);
 		num = 999;
@@ -19,6 +20,7 @@ public class DataType {
 		System.out.println(name);
 		System.out.println(isRun);
 		System.out.println(str);
+		System.out.println(name1);
 	}
 
 }
