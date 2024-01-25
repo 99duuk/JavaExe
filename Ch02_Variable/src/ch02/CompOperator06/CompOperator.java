ee63aac3fc6f9a017de54445a195828e85e32b8d
99duuk <99duuk@gmail.com>
2024-01-25 11:38:41 +0900

    num0 to num1 fix

--- Ch02_Variable/src/ch02/CompOperator06/CompOperator.java
+++ Ch02_Variable/src/ch02/CompOperator06/CompOperator.java
@@ -3,11 +3,10 @@ package ch02.CompOperator06;
 import java.util.Scanner;
 
 public class CompOperator {
-
 	public static void main(String[] args) {
 		boolean isLarge;
 		int num0, num1;
-		
+		 
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("[대소 비교 연산]");
@@ -17,10 +16,8 @@ public class CompOperator {
 		num1 = sc.nextInt();
 		
 		isLarge = num0 > num1;
-		System.out.println(num0 + "은 " + num1 + 
-				"보다 크다는 사실은 " + isLarge + "입니다.");
+		System.out.println(num0 + "은 " + num1 + "보다 크다는 사실은 " + isLarge + "입니다.");
 		sc.close();
-
+		
 	}
-
 }
